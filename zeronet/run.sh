7291161559ac7c99b68f307dae344495d0228514
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-04-29 19:38:02 +0200

    Remove not needed curly braces

--- zeronet/run.sh
+++ zeronet/run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 set -ex
-[ "${ENABLE_TOR}" = true ] && tor&
+[ "${ENABLE_TOR}" = true ] && tor &
 python zeronet.py --fileserver_port 26552 --ui_host "${UI_HOST}" --ui_ip 0.0.0.0 --ui_password "${UI_PASSWORD}"
