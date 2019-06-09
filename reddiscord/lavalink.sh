6f29451fb2b9c8ea7810a1f006fb8283d1958a6f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-06-09 12:48:53 +0200

    RedDiscord: Move common packages to their own layer (#21)
    * RedDiscord: Move common packages to their own layer
    
    Should fix the 50+ min build times hopefully
    
    * RedDiscord: Use official Lavalink repo
    
    * RedDiscord: Ignore libconnector.so
    
    * Add FOSSA check

--- reddiscord/lavalink.sh
+++ reddiscord/lavalink.sh
@@ -3,7 +3,7 @@
 set -eoux pipefail
 
 cd files
-curl -Lso Lavalink.jar "$(curl -s https://api.github.com/repos/Cog-Creators/Red-DiscordBot/releases?access_token="$GITHUB_TOKEN" | jq '[.[] | select(.assets[].name=="Lavalink.jar")][1] | .assets | .[0].browser_download_url' | tr -d \'\")"
+curl -Lso Lavalink.jar "$(curl -s https://api.github.com/repos/Frederikam/Lavalink/releases?access_token="$GITHUB_TOKEN" | jq '[.[] | select(.assets[].name=="Lavalink.jar")][1] | .assets | .[0].browser_download_url' | tr -d \'\")"
 mkdir -p natives/linux-arm/
 mv libconnector.so natives/linux-arm/
 zip Lavalink.jar natives/linux-arm/libconnector.so
