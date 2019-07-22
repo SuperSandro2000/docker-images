4c5cccc6847ff69f3652a3a31c120106106dc4d0
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-07-22 16:04:24 +0200

    RedDiscord: Copy .so instead of moving
    Saves git checkouts while manually testing

--- reddiscord/lavalink.sh
+++ reddiscord/lavalink.sh
@@ -5,6 +5,6 @@ set -eoux pipefail
 cd files
 curl -Lso Lavalink.jar "$(curl -s https://api.github.com/repos/Frederikam/Lavalink/releases?access_token="$GITHUB_TOKEN" | jq '[.[] | select(.assets[].name=="Lavalink.jar")][1] | .assets | .[0].browser_download_url' | tr -d \'\")"
 mkdir -p natives/linux-arm/
-mv libconnector.so natives/linux-arm/
+cp libconnector.so natives/linux-arm/
 zip Lavalink.jar natives/linux-arm/libconnector.so
 rm -r natives/
