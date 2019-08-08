70ec88283dcc9a22087ffaa1e6faec72fd243907
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-08-08 06:47:08 +0200

    Zeronet: Fix entrypoint, Make config vars optional

--- zeronet/files/run.sh
+++ zeronet/files/run.sh
@@ -1,4 +1,13 @@
 #! /bin/sh
 set -eou pipefail
+
+if [ -n "$UI_HOST" ] ; then
+    ARGS="${ARGS:-} --ui_host ${UI_HOST:-}"
+fi
+
+if [ -n "$UI_PASSWORD" ] ; then
+    ARGS="${ARGS:-} --ui_password ${UI_PASSWORD:-}"
+fi
+
 [ "${ENABLE_TOR:-}" = true ] && tor &
-python zeronet.py --fileserver_port 26552 --ui_host "${UI_HOST:-}" --ui_ip 0.0.0.0 --ui_password "${UI_PASSWORD:-}"
+python3 zeronet.py --fileserver_port 26552 --ui_ip 0.0.0.0 ${ARGS:-} "$@"
