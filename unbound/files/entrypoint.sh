dffc519dd2cdeaef1ab3eacd92c545453e216579
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-09-01 17:14:53 +0200

    Unbound: Run setup in entrypoint

--- unbound/files/entrypoint.sh
+++ unbound/files/entrypoint.sh
@@ -3,7 +3,7 @@ set -eou pipefail
 
 # if the first arg starts with "-" pass it to unbound
 if [ "${1#-}" != "$1" ]; then
-    set -- unbound "$@"
+    set -- unbound-control-setup && unbound "$@"
 fi
 
 if [ "$1" = "unbound" ] && [ "$(id -u)" = "0" ]; then
