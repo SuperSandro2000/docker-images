b9b39cdd16678a258f71791129e103ce121f06b3
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-09-05 13:27:41 +0200

    Unbound: Redo entrypoint

--- unbound/files/entrypoint.sh
+++ unbound/files/entrypoint.sh
@@ -3,12 +3,19 @@ set -eou pipefail
 
 # if the first arg starts with "-" pass it to unbound
 if [ "${1#-}" != "$1" ]; then
-    set -- unbound-control-setup && unbound "$@"
+  set -- unbound "$@"
 fi
 
 if [ "$1" = "unbound" ] && [ "$(id -u)" = "0" ]; then
-    find . \! -user unbound -exec chown unbound '{}' +
-    exec su-exec unbound "$0" "$@"
+  find . \! -user unbound -exec chown unbound '{}' +
+
+  if [[ ! -f /etc/unbound/unbound.conf ]]; then
+    su - unbound -s /bin/sh -c unbound-control-setup
+  fi
+
+  # this wouldn't work as we can't bind 53
+  #exec su-exec unbound "$0" "$@"
+  exec su-exec root "$@"
 fi
 
 exec "$@"
