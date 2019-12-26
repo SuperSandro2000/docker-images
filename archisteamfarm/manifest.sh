7a5945870dc121099e035930b0223e3729346e35
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-12-27 00:04:03 +0100

    ASF: Tag alpine releases correctly

--- archisteamfarm/manifest.sh
+++ archisteamfarm/manifest.sh
@@ -10,7 +10,9 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 
 $sudo rm ~/.docker/manifests/docker.io_supersandro2000_archisteamfarm-* -rf
 
+# latest pre-release or release
 release_version=$(curl -s https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/releases?access_token="$GITHUB_TOKEN" | jq -r '.[0] | .tag_name')
+# latest stable release
 latest_version=$(curl -s https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/releases/latest?access_token="$GITHUB_TOKEN" | jq -r '.tag_name')
 version=$(echo "$release_version" "$latest_version" | xargs -n1 | sort -u | xargs)
 
@@ -22,5 +24,11 @@ for variant in latest released $version; do
   retry "$sudo docker manifest push supersandro2000/archisteamfarm:$variant"
   sleep 3
 done
+
+$sudo docker manifest create supersandro2000/archisteamfarm:alpine-latest justarchi/archisteamfarm:"$latest_version"
+$sudo docker manifest annotate supersandro2000/archisteamfarm:alpine-latest justarchi/archisteamfarm:"$latest_version" --os linux --arch amd64
+
+retry "$sudo docker manifest push supersandro2000/archisteamfarm:$variant"
+sleep 3
 
 curl -X POST https://hooks.microbadger.com/images/supersandro2000/archisteamfarm/uFaZZ9Eb73O7_EOTXqHxPQls45E=
